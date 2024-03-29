891538f5ea4badacbf6fff088f99b47fc8cfcfb4
William Murray <William.murray1204@gmail.com>
2024-03-29 13:26:28 +1100

    Fix: Update pyproject file

--- tests/unload_model.py
+++ tests/unload_model.py
@@ -5,9 +5,6 @@ import faulthandler
 faulthandler.enable()
 
 MODEL_PATH = "C:\\stable-diffusion\\turbovisionxlSuperFastXLBasedOnNew_tvxlV431Bakedvae.q8_0.gguf"
-UPSCALER_MODEL_PATH = "C:\\stable-diffusion\\RealESRGAN_x4plus.pth"
-
-# stable_diffusion = StableDiffusion(upscaler_path=UPSCALER_MODEL_PATH, verbose=True)
 
 stable_diffusion = StableDiffusion(model_path=MODEL_PATH, verbose=True)
 
